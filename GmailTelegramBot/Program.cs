da9c74da2612d408765ec91529ffee2705230ae7
Anton Zhluktarov <anton.zhluktarov@gmail.com>
2020-01-02 19:08:53 +0200

    Initial commit

---
+++ GmailTelegramBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GmailTelegramBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
